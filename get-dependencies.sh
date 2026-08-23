8a4211148e80c737985789ce9b90aee88faef25c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-23 16:35:04 -0400

    refactor build, add `libdecor-mini`, build less often (#1)
    * refactor build, add `libdecor-mini`, build less often

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,45 +7,45 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cmake         \
-    fmt           \
-    libdecor      \
-    libzip        \
-    lsb-release   \
-    ninja         \
-    nlohmann-json \
-    opusfile      \
-    python        \
-    sdl2          \
-    sdl2_net      \
-    spdlog        \
-    tinyxml2
+	cmake         \
+	fmt           \
+	libzip        \
+	lsb-release   \
+	ninja         \
+	nlohmann-json \
+	opusfile      \
+	python        \
+	sdl2          \
+	sdl2_net      \
+	spdlog        \
+	tinyxml2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 make-aur-package zenity-rs-bin
 
 echo "Making stable build of 2ship2harkinian..."
 echo "---------------------------------------------------------------"
-REPO="https://github.com/HarbourMasters/2ship2harkinian"
-VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//')"
-git clone --branch "$VERSION" --single-branch --recursive --depth 1 "$REPO" ./2ship2harkinian
-echo "$VERSION" > ~/version
+REPO=https://github.com/HarbourMasters/2ship2harkinian
+VERSION=$(git ls-remote --tags --refs --sort=-v:refname "$REPO" | awk -F'/' '{print $NF; exit}')
 
-mkdir -p ./AppDir/bin/lib
-cd ./2ship2harkinian
-cmake . \
-    -Bbuild \
-    -GNinja
-cmake --build build --config Release
-cmake --build build --config Release --target Generate2ShipOtr
+git clone --branch "$VERSION" --single-branch --recursive --depth 1 "$REPO" ./2s2h && (
+	cd ./2s2h
 
-mv -v build/mm/assets ../AppDir/bin
-mv -v build/mm/2s2h.elf ../AppDir/bin
-mv -v build/mm/2ship.o2r ../AppDir/bin
-mv -v build/_deps/stormlib-build/libstorm.a ../AppDir/bin/lib
-wget -O ../AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
-cp -v build/2s2hIcon.png ../AppDir/.DirIcon
-mv -v build/2s2hIcon.png ../AppDir/2s2h.png
+	cmake ./ -Bbuild -GNinja
+	cmake --build build --config Release
+	cmake --build build --config Release --target Generate2ShipOtr
+
+	echo "$VERSION" > ~/version
+)
+
+mkdir -p ./AppDir/bin
+mv -v ./2s2h/build/mm/assets    ./AppDir/bin
+mv -v ./2s2h/build/mm/2s2h.elf  ./AppDir/bin
+mv -v ./2s2h/build/mm/2ship.o2r ./AppDir/bin
+mv -v ./2s2h/build/2s2hIcon.png ./AppDir/2s2h.png
+
+wget -O ./AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
+
