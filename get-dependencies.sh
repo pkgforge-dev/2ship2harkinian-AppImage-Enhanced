7c33a6c3d14381833bcdd030aa01fb3e07a19228
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-29 17:08:58 -0400

    make a release build (#3)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -34,7 +34,7 @@ VERSION=$(git ls-remote --tags --refs --sort=-v:refname "$REPO" | awk -F'/' '{pr
 git clone --branch "$VERSION" --single-branch --recursive --depth 1 "$REPO" ./2s2h && (
 	cd ./2s2h
 
-	cmake ./ -Bbuild -GNinja
+	cmake ./ -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release
 	cmake --build build --config Release -j$(nproc)
 	cmake --build build --config Release --target Generate2ShipOtr -j$(nproc)
 
