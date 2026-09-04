a9259b80c58918eeb3baf75479ef807b3a191332
Link4Electronics <link4electronics@gmail.com>
2026-09-04 20:27:08 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,18 +11,16 @@ pacman -Syu --noconfirm \
 	fmt           \
 	libzip        \
 	lsb-release   \
-	ninja         \
 	nlohmann-json \
 	opusfile      \
 	python        \
-	sdl2          \
 	sdl2_net      \
 	spdlog        \
 	tinyxml2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano libdecor-mini
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini opus-mini
 
 make-aur-package zenity-rs-bin
 
@@ -30,18 +28,14 @@ echo "Making stable build of 2ship2harkinian..."
 echo "---------------------------------------------------------------"
 REPO=https://github.com/HarbourMasters/2ship2harkinian
 VERSION=$(git ls-remote --tags --refs --sort=-v:refname "$REPO" | awk -F'/' '{print $NF; exit}')
+git clone --branch "$VERSION" --single-branch --recursive --depth 1 "$REPO"
+echo "$VERSION" > ~/version
 
-git clone --branch "$VERSION" --single-branch --recursive --depth 1 "$REPO" ./2s2h && (
-	cd ./2s2h
-
-	cmake ./ -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release
-	cmake --build build --config Release -j$(nproc)
-	cmake --build build --config Release --target Generate2ShipOtr -j$(nproc)
-
-	echo "$VERSION" > ~/version
-)
+cmake -S ./2s2h -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release -j$(nproc)
+cmake --build build --config Release --target Generate2ShipOtr -j$(nproc)
 
 mkdir -p ./AppDir/bin
-mv -v ./2s2h/build/mm/assets ./2s2h/build/mm/2s2h.elf ./2s2h/build/mm/2ship.o2r ./AppDir/bin
+mv -v ./build/mm/assets ./build/mm/2s2h.elf ./build/mm/2ship.o2r ./AppDir/bin
 wget -O ./AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
 
